ca0bcff2b9b3cab0c93bde1b1be272d5e7054ec4
Oliwer Banach <oliwerbanach@gmail.com>
2022-03-31 22:31:35 +0200

    master

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by obana on 31.03.2022.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by obana on 31.03.2022.
+//
+
+#ifndef CLION_MAIN_H
+#define CLION_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CLION_MAIN_H
